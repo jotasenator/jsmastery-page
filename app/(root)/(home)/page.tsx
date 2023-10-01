aa660439f84adb149a351412689730410275b654
Maykel <jotasenator@gmail.com>
2023-09-30 23:31:33 -0400

    Implement header messages

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -30,24 +30,32 @@ const Page = async ({ searchParams }: Props) => {
       </section>
       <Filters />
 
-      <section className="flex-center mt-6 w-full flex-col sm:mt-20">
-        <Header />
-        <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
-          {resources?.length > 0 ? (
-            resources.map((resource: any) => (
-              <ResourceCard
-                key={resource._id}
-                title={resource.title}
-                id={resource._id}
-                image={resource.image}
-                downloadNumber={resource.views}
-              />
-            ))
-          ) : (
-            <p>No resources found</p>
-          )}
-        </div>
-      </section>
+      {searchParams?.query || searchParams?.category ? (
+        <section className="flex-center mt-6 w-full flex-col sm:mt-20">
+          <Header
+            type="Resources"
+            query={searchParams?.query || ""}
+            category={searchParams?.category || ""}
+          />
+          <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+            {resources?.length > 0 ? (
+              resources.map((resource: any) => (
+                <ResourceCard
+                  key={resource._id}
+                  title={resource.title}
+                  id={resource._id}
+                  image={resource.image}
+                  downloadNumber={resource.views}
+                />
+              ))
+            ) : (
+              <p>No resources found</p>
+            )}
+          </div>
+        </section>
+      ) : (
+        ""
+      )}
     </main>
   );
 };
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,38 @@
 import React from "react";
 
-const Header = () => {
-  return <div>HEader</div>;
+type Props = {
+  query: string;
+  type: string;
+  category: string;
+};
+
+const Header = ({ type, query, category }: Props) => {
+  if (query && category) {
+    return (
+      <h1 className="heading3 self-start text-white-800 ">
+        Search results for "{query}" in{" "}
+        <span className="capitalize">{category}</span>
+      </h1>
+    );
+  }
+
+  if (query) {
+    return (
+      <h1 className="heading3 self-start text-white-800 ">
+        Search results for "{query}"
+      </h1>
+    );
+  }
+
+  if (category) {
+    return (
+      <h1 className="heading3 self-start text-white-800 ">
+        Search results for "{category}"
+      </h1>
+    );
+  }
+
+  return <h1 className="heading3 self-start text-white-800 ">No results</h1>;
 };
 
 export default Header;
